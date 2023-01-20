5397e23f0fd52e28a1ec1a8f926727da156ab261
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 19:13:14 +0530

    Should clear all Email Samples

--- MethodStoring.cs
+++ MethodStoring.cs
@@ -1,57 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//namespace UserRegistration
+//{
+//    public class MethodStoring
+//    {
+//        public static void ForFirstName()
+//        {
+//            Console.WriteLine("Rule for First Name:-\n1)only 3 character allowed.\n2)Only First char should capital");
+//            Console.WriteLine("\nEnter First Name : ");
+//            string Nm = Console.ReadLine();
+//            User UR = new User();
+//            UR.firstName(Nm);
+//        }
+//        public static void ForLatName()
+//        {
+//            Console.WriteLine("\nRule for Last Name:-\n1)only 3 character allowed.\n2)Only First char should capital");
+//            Console.WriteLine("\nEnter Last Name : ");
+//            string Lm = Console.ReadLine();
 
-namespace UserRegistration
-{
-    public class MethodStoring
-    {
-        public static void ForFirstName()
-        {
-            Console.WriteLine("Rule for First Name:-\n1)only 3 character allowed.\n2)Only First char should capital");
-            Console.WriteLine("\nEnter First Name : ");
-            string Nm = Console.ReadLine();
-            User UR = new User();
-            UR.firstName(Nm);
-        }
-        public static void ForLatName()
-        {
-            Console.WriteLine("\nRule for Last Name:-\n1)only 3 character allowed.\n2)Only First char should capital");
-            Console.WriteLine("\nEnter Last Name : ");
-            string Lm = Console.ReadLine();
+//            User UR = new User();
+//            UR.LastName(Lm);
+//        }
+//        public static void Foremail()
+//        {
 
-            User UR = new User();
-            UR.LastName(Lm);
-        }
-        public static void Foremail()
-        {
+//            Console.WriteLine("\nEnter Email : ");
+//            string Em = Console.ReadLine();
 
-            Console.WriteLine("\nEnter Email : ");
-            string Em = Console.ReadLine();
+//            User UR = new User();
+//            UR.email(Em);
+//        }
+//        public static void ForNumber()
+//        {
+//            Console.WriteLine("\nRule for Number:-\n1)entry code of 2 digit.\n2)give space after entry code\n3)only 10 integer allowed in mobile number.");
+//            Console.WriteLine("\nEnter Mobilr number with country code : ");
+//            string Mb = Console.ReadLine();
 
-            User UR = new User();
-            UR.email(Em);
-        }
-        public static void ForNumber()
-        {
-            Console.WriteLine("\nRule for Number:-\n1)entry code of 2 digit.\n2)give space after entry code\n3)only 10 integer allowed in mobile number.");
-            Console.WriteLine("\nEnter Mobilr number with country code : ");
-            string Mb = Console.ReadLine();
+//            User UR = new User();
+//            UR.mobile(Mb);
+//        }
+//        public static void ForPassword()
+//        {
+//            Console.WriteLine("\nRule for Password:-\n1)Minimum 8 character required.\n2)Atleast 1 char should be capital\n3)Atleast 1 char should be numeric");
+//            Console.WriteLine("\nEnter password : ");
+//            string Pw = Console.ReadLine();
 
-            User UR = new User();
-            UR.mobile(Mb);
-        }
-        public static void ForPassword()
-        {
-            Console.WriteLine("\nRule for Password:-\n1)Minimum 8 character required.\n2)Atleast 1 char should be capital\n3)Atleast 1 char should be numeric");
-            Console.WriteLine("\nEnter password : ");
-            string Pw = Console.ReadLine();
+//            User UR = new User();
+//            UR.PassWord(Pw);
+//        }
 
-            User UR = new User();
-            UR.PassWord(Pw);
-        }
-
-    }
-}
+//    }
+//}
