411c627f0cebb4ba51780b9c3a790880fd0e5369
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 11:01:56 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the User Regestration Program by Regex");
+
+        }
+
+    }
+}
