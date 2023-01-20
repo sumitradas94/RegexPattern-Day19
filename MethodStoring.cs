fb3bee3a974738c21c22bd2e89c0ae944fe2a303
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 11:17:21 +0530

    Method Stored for first name

---
+++ MethodStoring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class MethodStoring
+    {
+        public static void ForFirstName()
+        {
+            Console.WriteLine("Rule for First Name:-\n1)only 3 character allowed.\n2)Only First char should capital");
+            Console.WriteLine("\nEnter first name");
+            string Nm = Console.ReadLine();
+
+            User UR = new User();
+            UR.firstName(Nm);
+        }
+    }
+}
