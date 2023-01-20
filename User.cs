7871f06bc289c4ba05352af27be46d5500aaf64c
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 19:13:47 +0530

    Should clear all Email Samples

--- User.cs
+++ User.cs
@@ -7,80 +7,75 @@ using System.Threading.Tasks;
 
 namespace UserRegistration
 {
-    public class User
+    namespace UserRegistration
     {
-        bool a, b, c, d, e;
-        public static string Firstname = "^[A-Z]{1}[a-z]{2}$";
-        public static string Lastname = "^[A-Z]{1}[a-z]{2}$";
-        public static string Email = "^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*";
-        public static string Mob = "^[1-9]{2}[: :][0-9]{10}$";
-        public static string Pass = "^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$";
-        public void firstName(string Nm)
+        public class User
         {
-            a = Regex.IsMatch(Nm, Firstname);
+            public static string Reg_pattern1 = "^[A-Z]{1}[A-Za-z]{2,}$";
+            public static string Reg_pattern2 = "^[A-Z]{1}[A-Za-z]{2,}$";
+            public static string Reg_pattern3 = "^(abc)(.[A-Za-z]+)?@(bl).(co)([.][a-z]{2,})?$";
+            public static string Reg_pattern4 = "^[0-9]{2}\\s[0-9]{10}$";
+            public static string Reg_pattern5 = "^[a-zA-Z]{8,}([0-9]+)?$";
+            public static string Reg_pattern6 = "^[A-Z]{1,}[a-zA-Z]{7,}([0-9]+)?$";
+            public static string Reg_pattern7 = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}([0-9]+)?$";
+            public static string Reg_pattern8 = "^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$";
+            public static string Reg_pattern9 = "^[A-Za-z0-9]+([.+-][A-Za-z0-9]+)*@[A-Za-z0-9]+[.][a-z]{2,}([.][a-z]{2,})?$";
 
-            if (a)
+
+            //abc.xyz@bl.co.in
+
+
+            public static bool validateName(string name)
             {
-                Console.WriteLine("{0} is a valid Last name.", Nm);
+                return Regex.IsMatch(name, Reg_pattern1);
+
             }
-            else
+            public static bool validateLastName(string lastname)
             {
-                Console.WriteLine("{0} is not a valid Last name.", Nm);
+                return Regex.IsMatch(lastname, Reg_pattern2);
+            }
+
+            public static bool validateEmailID(string email)
+            {
+                return Regex.IsMatch(email, Reg_pattern3);
+            }
+            public static bool validateMobileNumber(string Monumber)
+            {
+                return Regex.IsMatch(Monumber, Reg_pattern4);
+
+            }
+
+            public static bool validatePassword(string password)
+            {
+                return Regex.IsMatch(password, Reg_pattern5);
+
+            }
+
+            public static bool validatePassword1(string password)
+            {
+                return Regex.IsMatch(password, Reg_pattern6);
+
+            }
+
+            public static bool validatePassword2(string password)
+            {
+                return Regex.IsMatch(password, Reg_pattern7);
+
+            }
+
+            public static bool validatePassword3(string password)
+            {
+                return Regex.IsMatch(password, Reg_pattern8);
+
+            }
+
+            public static bool validateEmailID1(string email)
+            {
+                return Regex.IsMatch(email, Reg_pattern9);
+
             }
         }
-        public void LastName(string Lm)
-        {
-            b = Regex.IsMatch(Lm, Lastname);
-
-            if (b)
-            {
-                Console.WriteLine("{0} is a valid name.", Lm);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a valid name.", Lm);
-            }
-        }
-        public void email(string Em)
-        {
-            c = Regex.IsMatch(Em, Email);
-
-            if (c)
-            {
-                Console.WriteLine("{0} is a valid Email.", Em);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a valid Email.", Em);
-            }
-        }
-        public void mobile(string Mb)
-        {
-            d = Regex.IsMatch(Mb, Mob);
-
-            if (d)
-            {
-                Console.WriteLine("{0} is a valid Mobile number.", Mb);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a valid Mobile number.", Mb);
-            }
-        }
-        public void PassWord(string Pw)
-        {
-            e = Regex.IsMatch(Pw, Pass);
-
-            if (e)
-            {
-                Console.WriteLine("{0} is a valid password.", Pw);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a valid Password.", Pw);
-            }
-        }
-
-
     }
 }
+
+
