ec5e123cbd38ff90174fda094b6eb714881af257
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 18:45:36 +0530

    Check predefined mobile format

--- User.cs
+++ User.cs
@@ -9,10 +9,11 @@ namespace UserRegistration
 {
     public class User
     {
-        bool a, b, c;
+        bool a, b, c, d;
         public static string Firstname = "^[A-Z]{1}[a-z]{2}$";
         public static string Lastname = "^[A-Z]{1}[a-z]{2}$";
         public static string Email = "^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*";
+        public static string Mob = "^[1-9]{2}[: :][0-9]{10}$";
         public void firstName(string Nm)
         {
             a = Regex.IsMatch(Nm, Firstname);
@@ -50,6 +51,19 @@ namespace UserRegistration
             else
             {
                 Console.WriteLine("{0} is not a valid Email.", Em);
+            }
+        }
+        public void mobile(string Mb)
+        {
+            d = Regex.IsMatch(Mb, Mob);
+
+            if (d)
+            {
+                Console.WriteLine("{0} is a valid Mobile number.", Mb);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid Mobile number.", Mb);
             }
         }
 
