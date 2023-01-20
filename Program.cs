4b3f082f5892bd176ab2c86f18cf08217f2a1e84
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 11:17:52 +0530

    Method calling for Uc1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the User Regestration Program by Regex");
+            MethodStoring.ForFirstName();
+
 
         }
 
