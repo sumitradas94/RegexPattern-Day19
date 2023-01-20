ebd92190450c38345bb432a70e08d32134c9cdb6
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 11:15:40 +0530

    Added first user info

---
+++ User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class User
+    {
+        public static string Firstname = "^[A-Z]{1}[a-z]{2}$";
+
+        public void firstName(string Nm)
+        {
+            bool a = Regex.IsMatch(Nm, Firstname);
+
+            if (a)
+            {
+                Console.WriteLine("{0} is a valid name.", Nm);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid name.", Nm);
+
+
+
+            }
+        }
+    }
+}
+
