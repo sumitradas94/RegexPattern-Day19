bf4eb1c0abeabc51ff7f8d5f5eb646247d91c1ba
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 18:14:29 +0530

    Check valid Email

--- User.cs
+++ User.cs
@@ -9,26 +9,26 @@ namespace UserRegistration
 {
     public class User
     {
-        bool a, b;
+        bool a, b, c;
         public static string Firstname = "^[A-Z]{1}[a-z]{2}$";
         public static string Lastname = "^[A-Z]{1}[a-z]{2}$";
-
+        public static string Email = "^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*";
         public void firstName(string Nm)
         {
             a = Regex.IsMatch(Nm, Firstname);
 
             if (a)
             {
-                Console.WriteLine("{0} is a valid name.", Nm);
+                Console.WriteLine("{0} is a valid Last name.", Nm);
             }
             else
             {
-                Console.WriteLine("{0} is not a valid name.", Nm);
+                Console.WriteLine("{0} is not a valid Last name.", Nm);
             }
         }
         public void LastName(string Lm)
         {
-            b = Regex.IsMatch(Lm, Firstname);
+            b = Regex.IsMatch(Lm, Lastname);
 
             if (b)
             {
@@ -39,6 +39,19 @@ namespace UserRegistration
                 Console.WriteLine("{0} is not a valid name.", Lm);
             }
         }
+        public void email(string Em)
+        {
+            c = Regex.IsMatch(Em, Email);
+
+            if (c)
+            {
+                Console.WriteLine("{0} is a valid Email.", Em);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a valid Email.", Em);
+            }
+        }
+
     }
 }
-
